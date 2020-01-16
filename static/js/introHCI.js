e5a9bc43e2b3d2f7824054987397a84f778ee250
Jiayu Fu <j3fu@ucsd.edu>
2020-01-16 15:26:18 -0800

    Replaced default information in web page

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
-            $("a.thumbnail").click(projectClick);
             });
+    $("a.thumbnail").click(projectClick);
 }
 function projectClick(e){
 	// prevent the page from reloading
-	e.preventDefault()
+	e.preventDefault();
 	// In an event handler,$(this) refers to 
 	// the object that triggered the event
-	$(this).css("background-color",:#7fff00");
+	$(this).css("background-color","#7fff00");
 }
